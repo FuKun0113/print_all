d1a85625b5e5cea925ac63d4826f2c28f8e46d7b
FuKun0113 <a2435454985@gmail.com>
2023-08-02 20:39:24 +0800

    初步完成功能

--- main.py
+++ main.py
@@ -11,9 +11,25 @@
 # pip install pywin32
 
 # 引入模块
-
+import tkinter
 import customtkinter
+import win32print
+import win32api
+import os
 
+# 功能部分
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# 获取所有打印机
+printers = []
+for it in win32print.EnumPrinters(3):
+    printers.append(it[2])
+print(printers)
+# 获取默认打印机
+printer = win32print.GetDefaultPrinter()
+print(printer)
+
+# GUI部分
+# ---------------------------------------------------------------------------------------------------------------------------------------
 # 主题风格: "System" (standard), "Dark", "Light"
 customtkinter.set_appearance_mode("System")
 # 主题颜色: "blue" (standard), "green", "dark-blue"
@@ -42,28 +58,59 @@ class App(customtkinter.CTk):
         self.tool_bar = customtkinter.CTkFrame(master=self)
         self.tool_bar.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
         # 选择打印机
-        self.sel_printer = customtkinter.CTkOptionMenu(master=self.tool_bar, values=["打印机1", "打印机2"])
+        self.sel_printer = customtkinter.CTkOptionMenu(master=self.tool_bar, values=printers)
         self.sel_printer.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+        self.sel_printer.set(printer)
         # 选择文件夹
         self.sel_path = customtkinter.CTkEntry(master=self.tool_bar, placeholder_text="请选择文件夹")
         self.sel_path.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
         # 选择文件夹按钮
-        self.sel_file = customtkinter.CTkButton(master=self.tool_bar, text="选择文件夹")
+        self.sel_file = customtkinter.CTkButton(master=self.tool_bar, text="📂", width=50, command=self.file_path)
         self.sel_file.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
         # 读取文件列表按钮
-        self.read_list = customtkinter.CTkButton(master=self.tool_bar, text="读取列表")
+        self.read_list = customtkinter.CTkButton(master=self.tool_bar, text="读取列表", command=self.load_list)
         self.read_list.grid(row=0, column=3, padx=10, pady=10, sticky="nsew")
 
         # 文件列表区
-        self.list_area = customtkinter.CTkFrame(master=self)
+        self.list_area = customtkinter.CTkScrollableFrame(master=self)
         self.list_area.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+        # 文件名
+        self.file_name = customtkinter.CTkLabel(master=self.list_area, text="文件名")
+        self.file_name.grid(row=0, column=0, padx=10)
+        # 文件格式
+        self.ex_name = customtkinter.CTkLabel(master=self.list_area, text="文件格式")
+        self.ex_name.grid(row=0, column=1, padx=10)
+        # 单面双面
+        self.label2 = customtkinter.CTkLabel(master=self.list_area, text="单面双面")
+        self.label2.grid(row=0, column=2, padx=10)
+        # 打印份数
+        self.label3 = customtkinter.CTkLabel(master=self.list_area, text="打印份数")
+        self.label3.grid(row=0, column=3, padx=10)
 
         # 按钮区
         self.print_btn = customtkinter.CTkButton(master=self, text="打印")
         self.print_btn.grid(row=3, column=0, padx=10, pady=10, sticky="ns")
 
+    # 函数区
+    # ---------------------------------------------------------------------------------------------------------------------------------------
+    # 选择文件夹函数
+    def file_path(self):
+        dir_path = tkinter.filedialog.askdirectory()
+        self.sel_path.delete(0, "end")
+        self.sel_path.insert(0, dir_path)
+
+    # 读取文件列表函数
+    def load_list(self):
+        path = self.sel_path.get()
+        files = os.listdir(path)  # 得到文件夹下的所有文件名称
+        i = 1
+        for file in files:  # 遍历文件夹
+            self.file_title = customtkinter.CTkLabel(master=self.list_area,text=file)
+            self.file_title.grid(row=i, column=0)
+            i += 1
+        
+
 # 运行程序
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-
